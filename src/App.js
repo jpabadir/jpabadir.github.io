a66ad9a809ccd02dae28e6970b86b65690c32c1b
Jean-Philippe Abadir <jpabadir@gmail.com>
2021-11-17 01:37:29 -0500

    Add pic of me

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
+import me from './assets/picofme.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={me} className="App-logo" alt="logo" />
         <p>
           This is JP's app
         </p>
