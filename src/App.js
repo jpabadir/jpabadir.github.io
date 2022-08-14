0399bf2e6a3b7ba799073585663108f7c5810082
Jean-Philippe Abadir <jpabadir@gmail.com>
2022-08-13 23:27:42 -0400

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import me from './assets/picofme.jpeg';
+import React, { useState } from 'react';
 import './App.css';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+// import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
 function App() {
+  const [navItems, setNavItems] = useState(['coding', 'content', 'music', 'korean', 'blog']);
+  // function handleDrawerToggle() {
+  //   console.log('Test');
+  // }
   return (
     <div className="App">
-      <header className="App-header">
-        <div>Jean-Philippe Abadir</div>
-        <img src={me} className="ProfilePicture" alt="logo" />
-        <a href="article-1">article 1</a>
-        <a className="App-link" href="https://github.com/jpabadir" target="_blank" rel="noopener noreferrer">
-          My GitHub
-        </a>
-      </header>
+      <AppBar component="nav">
+        <Toolbar sx={{ backgroundColor: 'black' }}>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {navItems.map((item) => (
+              <Button key={item} href={item} sx={{ color: '#fff' }}>
+                {item}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
