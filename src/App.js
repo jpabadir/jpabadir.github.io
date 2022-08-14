4e249b4e80176732da8580f80512fff4b8c1883f
Jean-Philippe Abadir <jpabadir@gmail.com>
2022-08-13 23:33:23 -0400

    Add spotify

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 
 function App() {
-  const [navItems, setNavItems] = useState(['coding', 'content', 'music', 'korean', 'investing', 'blog']);
+  const [navItems, setNavItems] = useState(['coding', 'content', 'music', 'spotify', 'korean', 'investing', 'blog']);
   // function handleDrawerToggle() {
   //   console.log('Test');
   // }
