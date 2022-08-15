1879eee3c09171e6e9a551389971557a5dfdc603
Jean-Philippe Abadir <jpabadir@gmail.com>
2022-08-14 20:47:45 -0400

    Deploy build - Add blogs data file

--- src/routes/blog.js
+++ src/routes/blog.js
@@ -1,16 +1,17 @@
 import { NavLink, Outlet } from 'react-router-dom';
+import blogsData from '../blogs.json';
 
 export default function Blog() {
   return (
     <div>
       <div>
-        <NavLink to="article-1">article 1</NavLink>
-      </div>
-      <div>
-        <NavLink to="article-2">article 2</NavLink>
-      </div>
-      <div>
-        <NavLink to="article-3">article 3</NavLink>
+        {blogsData.map((blog) => (
+          <div>
+            <NavLink style={{ color: 'white' }} to={blog.title.replace(/\s+/g, '-').toLowerCase()}>
+              {blog.title}
+            </NavLink>
+          </div>
+        ))}
       </div>
       <Outlet />
     </div>
