7544b99c052612b3aba8381eef99b54b251ae637
Jean-Philippe Abadir <jpabadir@gmail.com>
2022-08-28 09:10:14 -0400

    Implement basic books page

--- src/routes/books-i-read.js
+++ src/routes/books-i-read.js
@@ -1,19 +1,66 @@
 import { NavLink, Outlet } from 'react-router-dom';
-import blogsData from '../blogs.json';
+import books from '../books.json';
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 export default function Books() {
   return (
     <div>
-      <div class="navbar">
-        {blogsData.map((blog) => (
-          <div key={blog.title}>
-            <NavLink className="NavLink" to={blog.title.replace(/\s+/g, '-').toLowerCase()}>
-              {blog.title}
-            </NavLink>
+      <table>
+        <tr>
+          <th>Author</th>
+          <th>Title</th>
+          <th>Date Started</th>
+          <th>Date Finished</th>
+        </tr>
+        {books.map((book) => (
+          <div key={book.title}>
+            <tr>
+              <td>{book.title}</td>
+              <td>{book.author}</td>
+              <td>{book.dateStarted}</td>
+              <td>{book.dateFinished}</td>
+            </tr>
           </div>
         ))}
-      </div>
-      <Outlet />
+      </table>
     </div>
   );
 }
+
+
+{/* <TableContainer component={Paper}>
+<Table sx={{ minWidth: 650 }} aria-label="simple table">
+  <TableHead>
+    <TableRow>
+      <TableCell>Dessert (100g serving)</TableCell>
+      <TableCell align="right">Calories</TableCell>
+      <TableCell align="right">Fat&nbsp;(g)</TableCell>
+      <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+      <TableCell align="right">Protein&nbsp;(g)</TableCell>
+    </TableRow>
+  </TableHead>
+  <TableBody>
+    {rows.map((row) => (
+      <TableRow
+        key={row.name}
+        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+      >
+        <TableCell component="th" scope="row">
+          {row.name}
+        </TableCell>
+        <TableCell align="right">{row.calories}</TableCell>
+        <TableCell align="right">{row.fat}</TableCell>
+        <TableCell align="right">{row.carbs}</TableCell>
+        <TableCell align="right">{row.protein}</TableCell>
+      </TableRow>
+    ))}
+  </TableBody>
+</Table>
+</TableContainer> */}
