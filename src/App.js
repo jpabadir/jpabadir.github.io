e513c9922a552817dae52d25d9256b17a3dc98f0
Jean-Philippe Abadir <jpabadir@gmail.com>
2022-08-28 10:22:48 -0400

    Make table dark

--- src/App.js
+++ src/App.js
@@ -2,27 +2,43 @@ import React from 'react';
 import './App.css';
 import { Outlet, NavLink } from 'react-router-dom';
 import linkify from './helpers';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
 
 function App() {
-  const navItems = [{ name: 'Home', link: '/' }, { name: 'YouTube', link: 'https://www.youtube.com/channel/UCliwHW2THGTsKwNvNcF_Nlw' }, { name: 'Blog' }, { name: "Books I've read", link: 'books-i-read' }];
+  const navItems = [
+    { name: 'Home', link: '/' },
+    { name: 'YouTube', link: 'https://www.youtube.com/channel/UCliwHW2THGTsKwNvNcF_Nlw' },
+    { name: 'Blog' },
+    { name: "Books I've read", link: 'books-i-read' },
+  ];
   return (
-    <div className="App">
-      <div className="navbar">
-        {navItems.map((item) => {
-          return item.link && item.link.includes('https') ? (
-            // eslint-disable-next-line react/jsx-no-target-blank
-            <a key={item.name} target="_blank" rel="noopener" className="NavLink" href={item.link}>
-              {item.name}
-            </a>
-          ) : (
-            <NavLink key={item.name} to={item.link ? item.link : linkify(item.name)} className="NavLink">
-              {item.name}
-            </NavLink>
-          );
-        })}
+    <ThemeProvider
+      theme={createTheme({
+        palette: {
+          mode: 'dark',
+        },
+      })}
+    >
+      <div className="App">
+        <div className="navbar">
+          {navItems.map((item) => {
+            return item.link && item.link.includes('https') ? (
+              // eslint-disable-next-line react/jsx-no-target-blank
+              <a key={item.name} target="_blank" rel="noopener" className="NavLink" href={item.link}>
+                {item.name}
+              </a>
+            ) : (
+              <NavLink key={item.name} to={item.link ? item.link : linkify(item.name)} className="NavLink">
+                {item.name}
+              </NavLink>
+            );
+          })}
+        </div>
+        <Outlet />
       </div>
-      <Outlet />
-    </div>
+    </ThemeProvider>
   );
 }
 
