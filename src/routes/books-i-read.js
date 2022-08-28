3d2b8636a3960fb0c394bc8f7310d3bb7396035d
Jean-Philippe Abadir <jpabadir@gmail.com>
2022-08-28 11:10:42 -0400

    lint

--- src/routes/books-i-read.js
+++ src/routes/books-i-read.js
@@ -16,35 +16,19 @@ export default function Books() {
         <Table aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell>
-                Author
-              </TableCell>
-              <TableCell>
-                Title
-              </TableCell>
-              <TableCell>
-                Date Started
-              </TableCell>
-              <TableCell>
-                Date Finished
-              </TableCell>
+              <TableCell>Author</TableCell>
+              <TableCell>Title</TableCell>
+              <TableCell>Date Started</TableCell>
+              <TableCell>Date Finished</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {books.map((row) => (
               <TableRow key={row.title} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                <TableCell label="Author">
-                  {row.author}
-                </TableCell>
-                <TableCell label="Title">
-                  {row.title}
-                </TableCell>
-                <TableCell label="Date Started">
-                  {row.dateStarted}
-                </TableCell>
-                <TableCell label="Date Finished">
-                  {row.dateFinished}
-                </TableCell>
+                <TableCell label="Author">{row.author}</TableCell>
+                <TableCell label="Title">{row.title}</TableCell>
+                <TableCell label="Date Started">{row.dateStarted}</TableCell>
+                <TableCell label="Date Finished">{row.dateFinished}</TableCell>
               </TableRow>
             ))}
           </TableBody>
