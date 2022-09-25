da548e3a30b3e6a61d7002827495e6ae68513050
Jean-Philippe Abadir <jpabadir@gmail.com>
2022-09-25 11:02:18 -0400

    Update

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,5 +1,6 @@
 import { NavLink } from 'react-router-dom';
 import blogsData from '../../data/blogs/blogs-headers.json';
+import './Blog.css';
 
 export default function Blog() {
   return (
@@ -7,10 +8,10 @@ export default function Blog() {
       <div>
         {blogsData.map((blog) => (
           <div key={blog.title}>
-            <NavLink className="Link NavLink container my-2" to={blog.title.replace(/\s+/g, '-').toLowerCase()}>
+            <NavLink className="Link BlogLink container my-2" to={blog.title.replace(/\s+/g, '-').toLowerCase()}>
               <div className="row">
                 <div className="col-lg-6">
-                  <img style={{ width: '350px', height: '200px', borderRadius: '10px' }} src={blog.image} alt={blog.title} />
+                  <img className='BlogThumbnail' src={blog.image} alt={blog.title} />
                 </div>
                 <div className="col-lg-6 d-flex align-items-center justify-content-center">
                   <div className="text-center">{blog.title}</div>
