872968f3fa94c12f64065c3e3c32dcd9ed422c88
Jean-Philippe Abadir <jpabadir@gmail.com>
2022-09-24 21:18:18 -0400

    Deploy build - Update

--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -15,25 +15,25 @@ export default function Books() {
         <Table aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell sx={{ color: 'inherit', fontWeight: 'bold' }}>Author</TableCell>
-              <TableCell sx={{ color: 'inherit', fontWeight: 'bold' }}>Title</TableCell>
-              <TableCell sx={{ color: 'inherit', fontWeight: 'bold' }}>Date Started</TableCell>
-              <TableCell sx={{ color: 'inherit', fontWeight: 'bold' }}>Date Finished</TableCell>
+              <TableCell sx={{ color: 'inherit', fontSize: 'inherit', fontWeight: 'bold' }}>Author</TableCell>
+              <TableCell sx={{ color: 'inherit', fontSize: 'inherit', fontWeight: 'bold' }}>Title</TableCell>
+              <TableCell sx={{ color: 'inherit', fontSize: 'inherit', fontWeight: 'bold' }}>Date Started</TableCell>
+              <TableCell sx={{ color: 'inherit', fontSize: 'inherit', fontWeight: 'bold' }}>Date Finished</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {books.map((row) => (
               <TableRow key={row.title} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                <TableCell sx={{ color: 'inherit' }} label="Title">
+                <TableCell sx={{ color: 'inherit', fontSize: 'inherit' }} label="Title">
                   {row.title}
                 </TableCell>
-                <TableCell sx={{ color: 'inherit' }} label="Author">
+                <TableCell sx={{ color: 'inherit', fontSize: 'inherit' }} label="Author">
                   {row.author}
                 </TableCell>
-                <TableCell sx={{ color: 'inherit' }} label="Date Started">
+                <TableCell sx={{ color: 'inherit', fontSize: 'inherit' }} label="Date Started">
                   {row.dateStarted}
                 </TableCell>
-                <TableCell sx={{ color: 'inherit' }} label="Date Finished">
+                <TableCell sx={{ color: 'inherit', fontSize: 'inherit' }} label="Date Finished">
                   {row.dateFinished}
                 </TableCell>
               </TableRow>
