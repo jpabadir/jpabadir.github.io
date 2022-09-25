1e60731aa803e82433caf69d732a72e003dc7172
Jean-Philippe Abadir <jpabadir@gmail.com>
2022-09-25 10:58:10 -0400

    Deploy build - Update

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -7,13 +7,13 @@ export default function Blog() {
       <div>
         {blogsData.map((blog) => (
           <div key={blog.title}>
-            <NavLink className="Link NavLink container" to={blog.title.replace(/\s+/g, '-').toLowerCase()}>
+            <NavLink className="Link NavLink container my-2" to={blog.title.replace(/\s+/g, '-').toLowerCase()}>
               <div className="row">
                 <div className="col-lg-6">
                   <img style={{ width: '350px', height: '200px', borderRadius: '10px' }} src={blog.image} alt={blog.title} />
                 </div>
-                <div className="col-lg-6 d-flex align-items-center">
-                  <div>{blog.title}</div>
+                <div className="col-lg-6 d-flex align-items-center justify-content-center">
+                  <div className="text-center">{blog.title}</div>
                 </div>
               </div>
             </NavLink>
