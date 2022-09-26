6c9704d55c9ab3b620b18b869c24d90fb286babc
Jean-Philippe Abadir <jpabadir@gmail.com>
2022-09-25 23:38:58 -0400

    Deploy build -

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,4 +1,10 @@
+import { useEffect } from 'react';
+
 export default function Countries() {
+  useEffect(() => {
+    window.drawRegionsMap();
+  }, []);
+
   return (
     <div>
       <p>Layovers don't count, and neither does merely driving / passing through a country on one's way to another.</p>
