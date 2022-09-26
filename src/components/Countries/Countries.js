8d491fe7a599f5c5379c65d396db7297f88c9acc
Jean-Philippe Abadir <jpabadir@gmail.com>
2022-09-25 23:43:54 -0400

    Deploy build -

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -8,7 +8,7 @@ export default function Countries() {
   return (
     <div>
       <p>Layovers don't count, and neither does merely driving / passing through a country on one's way to another.</p>
-      <div id="regions_div" style={{ widht: '100%' }} />
+      <div id="regions_div" style={{ height: window.innerWidth >= 768 ? '70vh' : '' }} />
     </div>
   );
 }
