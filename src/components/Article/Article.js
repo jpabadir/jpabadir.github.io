c3f98138569565f2ed68aa12a1592f72d51550ac
Jean-Philippe Abadir <jpabadir@gmail.com>
2022-10-02 11:29:00 -0400

    Deploy build -

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -6,13 +6,17 @@ export default function Article(props) {
   const ComponentToRender = require(`../../data/blogs/${linkify(props.blog.title)}`).default;
   return (
     <div className="Article">
-      <h1 className="text-center">{props.blog.title}</h1>
-      <h2 className="text-center">{props.blog.subtitle}</h2>
-      <div>{props.blog.publishDate}</div>
-      <div className="d-flex justify-content-center">
-        <img className="ArticleImage" src={props.blog.image} alt={props.blog.title} />
+      <div style={{ maxWidth: '1100px' }}>
+        <div className="text-left mb-4">
+          <h1>{props.blog.title}</h1>
+          <h2 className="Subtitle">{props.blog.subtitle}</h2>
+          <div className="Date">{new Date(props.blog.publishDate).toDateString()}</div>
+        </div>
+        <div className="d-flex justify-content-center mb-4">
+          <img className="ArticleImage" src={props.blog.image} alt={props.blog.title} />
+        </div>
+        <ComponentToRender />
       </div>
-      <ComponentToRender />
     </div>
   );
 }
