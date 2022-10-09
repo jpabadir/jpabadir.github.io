f6c2851f8beb83823a6f1e2d67bcd6abae445824
Jean-Philippe Abadir <jpabadir@gmail.com>
2022-10-08 22:56:26 -0400

    Deploy build -

--- src/components/Newsletter/Newsletter.js
+++ src/components/Newsletter/Newsletter.js
@@ -1,5 +1,10 @@
 import { formCode } from './convert-kit-form';
+import './Newsletter.css';
 
 export default function Newsletter() {
-  return <div dangerouslySetInnerHTML={{ __html: formCode }} />;
+  return (
+    <div className="Newsletter">
+      <div className="NewsletterForm" dangerouslySetInnerHTML={{ __html: formCode }} />
+    </div>
+  );
 }
