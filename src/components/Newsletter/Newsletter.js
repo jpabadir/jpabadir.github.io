357e9d3a7e9164d80b9adb191eec6f58b25a8879
Jean-Philippe Abadir <jpabadir@gmail.com>
2022-10-08 23:25:14 -0400

    Deploy build -

--- src/components/Newsletter/Newsletter.js
+++ src/components/Newsletter/Newsletter.js
@@ -9,7 +9,7 @@ export default function Newsletter() {
       pretty protective with my inbox, so don't feel pressured to join this. Personally, I'm pretty protective with my inbox, so don't feel pressured to join this. Personally, I'm pretty protective
       with my inbox, so don't feel pressured to join this. Personally, I'm pretty protective with my inbox, so don't feel pressured to join this.
       <div className="Newsletter">
-        <div className="NewsletterForm justify-content-between" dangerouslySetInnerHTML={{ __html: formCode }} />
+        <div dangerouslySetInnerHTML={{ __html: formCode }} />
       </div>
     </div>
   );
