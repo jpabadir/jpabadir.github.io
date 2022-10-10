30a3613d6b2231d52bf0f1cd44580a66118bb988
Jean-Philippe Abadir <jpabadir@gmail.com>
2022-10-09 20:24:57 -0400

    Deploy build -

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -3,18 +3,18 @@ import blogsData from '../../data/blogs/blogs-headers.json';
 import './Blog.css';
 
 export default function Blog() {
-  function myFunction(event) {
-    console.log(event.target.getElementsByTagName('img'));
+  function toggleScaleThumbnail(event) {
+    event.currentTarget.getElementsByTagName('img')[0].classList.toggle('BlogThumbnailScaled');
   }
 
   return (
     <div className="Blog">
       {blogsData.map((blog) => (
-        <div key={blog.title} onMouseEnter={myFunction}>
+        <div key={blog.title} onMouseEnter={toggleScaleThumbnail} onMouseLeave={toggleScaleThumbnail}>
           <NavLink className="Link BlogLink container my-2" to={blog.title.replace(/\s+/g, '-').toLowerCase()}>
             <div className="row">
               <div className="col-lg-6">
-                <div className="BlogImageParent">
+                <div className="BlogThumbnailParent">
                   <img className="BlogThumbnail" src={blog.image} alt={blog.title} />
                 </div>
               </div>
