5b3c24d3df8735c1d360f810a9e86e3955944d63
JP Abadir <jpabadir@gmail.com>
2024-07-14 23:07:27 -0400

    Deploy

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,52 +1,57 @@
 import { useEffect, useState } from "react";
-import Map from './Countries.svg';
+import Map from "./Countries.svg";
 
 export default function Countries() {
   const [loaded, setLoaded] = useState(false);
+  const countries = [
+    "France",
+    "United States",
+    "Canada",
+    "France",
+    "Morocco",
+    "Tunisia",
+    "Egypt",
+    "Switzerland",
+    "Germany",
+    "United Kingdom",
+    "Spain",
+    "Portugal",
+    "Italy",
+    "Ireland",
+    "Netherlands",
+    "Taiwan",
+  ];
 
   useEffect(() => {
-    const scriptTag = document.createElement('script');
-    scriptTag.src = 'https://www.gstatic.com/charts/loader.js';
-    scriptTag.addEventListener('load', () => setLoaded(true));
+    const scriptTag = document.createElement("script");
+    scriptTag.src = "https://www.gstatic.com/charts/loader.js";
+    scriptTag.addEventListener("load", () => setLoaded(true));
     document.body.appendChild(scriptTag);
   }, []);
 
   useEffect(() => {
     if (!loaded) return;
 
-    window.google.charts.load('current', {
-      packages: ['geochart'],
+    window.google.charts.load("current", {
+      packages: ["geochart"],
     });
     window.google.charts.setOnLoadCallback(drawRegionsMap);
 
     function drawRegionsMap() {
       var data = window.google.visualization.arrayToDataTable([
-        ['Country', 'Whether Ive been'],
-        ['France', 1],
-        ['United States', 1],
-        ['Canada', 1],
-        ['France', 1],
-        ['Morocco', 1],
-        ['Tunisia', 1],
-        ['Egypt', 1],
-        ['Switzerland', 1],
-        ['Germany', 1],
-        ['United Kingdom', 1],
-        ['Spain', 1],
-        ['Portugal', 1],
-        ['Italy', 1],
-        ['Ireland', 1],
-        ['Netherlands', 1],
-        ['Taiwan', 1],
+        ["Country", "Whether Ive been"],
+        ...countries.map((country) => [country, 1]),
       ]);
 
       var options = {
-        colorAxis: { colors: ['#e31b23'] },
-        legend: 'none',
+        colorAxis: { colors: ["#e31b23"] },
+        legend: "none",
         enableRegionInteractivity: false,
       };
 
-      var chart = new window.google.visualization.GeoChart(document.getElementById('regions_div'));
+      var chart = new window.google.visualization.GeoChart(
+        document.getElementById("regions_div")
+      );
 
       chart.draw(data, options);
     }
@@ -54,9 +59,14 @@ export default function Countries() {
 
   return (
     <div>
+      <div className="mb-3">
+        <span style={{ color: "red" }}>{countries.length}</span> countries so
+        far
+      </div>
       <div id="regions_div" />
       {/* <img src={Map} alt="A map of the countries I've been to" style={{ width: '100%', height: '100%' }} /> */}
-      Layovers don't count, and neither does merely driving / passing through a country on one's way to another.
+      Layovers don't count, and neither does merely driving / passing through a
+      country on one's way to another.
     </div>
   );
 }
