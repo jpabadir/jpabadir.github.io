0563bc7f7abe0807dcd301688906bd709879dcd5
JP Abadir <jpabadir@gmail.com>
2024-11-02 21:36:49 -0400

    Deploy

--- src/components/Athletics/Athletics.js
+++ src/components/Athletics/Athletics.js
@@ -9,11 +9,25 @@ export default function NoFights() {
   return (
     <div>
       <h3>Weightlifting</h3>
-      <div><strong>Squat</strong>: 5 sets of 5 reps at 185 lbs</div>
-      <div><strong>Bench press</strong>: 5 sets of 5 reps at 185 lbs</div>
-      <div><strong>Romanian deadlift</strong>: 5 sets of 5 reps at 185 lbs</div>
-      <div><strong>Overhead press</strong>: 5 sets of 5 reps at 85 lbs</div>
-      <div><strong>Barbell Curl</strong>: 5 sets of 5 reps at 70 lbs</div>
+      <div>
+        <strong>Squat</strong>: 5 sets of 5 reps at 185 lbs
+      </div>
+      <div>
+        <strong>Bench press</strong>: 5 sets of 5 reps at 185 lbs
+      </div>
+      <div>
+        <strong>Romanian deadlift</strong>: 5 sets of 5 reps at 185 lbs
+      </div>
+      <div>
+        <strong>Overhead press</strong>: 5 sets of 5 reps at 85 lbs
+      </div>
+      <div>
+        <strong>Barbell Curl</strong>: 5 sets of 5 reps at 70 lbs
+      </div>
+      <h3 className="mt-3">Running</h3>
+      <div>
+        <strong>Half marathon record</strong>: 02:10:33 on 2024-10-20
+      </div>
     </div>
   );
 }
