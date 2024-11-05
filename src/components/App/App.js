02bb614197c541c10022692ba97e5ed9771fc445
JP Abadir <jpabadir@gmail.com>
2024-11-05 01:41:28 -0500

    Deploy

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import './App.css';
-import { Outlet, NavLink } from 'react-router-dom';
-import { linkify } from '../../helpers';
-import Footer from '../Footer/Footer';
+import React from "react";
+import "./App.css";
+import { Outlet, NavLink } from "react-router-dom";
+import { linkify } from "../../helpers";
+import Footer from "../Footer/Footer";
 
 function App() {
   const navItems = [
-    { name: 'Home', link: '/' },
+    { name: "Home", link: "/" },
     // { name: 'YouTube', link: 'https://www.youtube.com/channel/UCliwHW2THGTsKwNvNcF_Nlw' },
     // { name: 'Articles' },
     // { name: "Books I've read", link: 'books-i-read' },
@@ -21,17 +21,28 @@ function App() {
     <div className="App">
       <div className="customNavbarClass">
         {navItems.map((item) => {
-          return item.link && item.link.includes('https') ? (
+          return item.link && item.link.includes("https") ? (
             // eslint-disable-next-line react/jsx-no-target-blank
-            <a key={item.name} target="_blank" rel="noopener" className="Link NavLink" href={item.link}>
+            <a
+              key={item.name}
+              target="_blank"
+              rel="noopener"
+              className="Link NavLink"
+              href={item.link}
+            >
               {item.name}
             </a>
           ) : (
-            <NavLink key={item.name} to={item.link ? item.link : linkify(item.name)} className="Link NavLink">
+            <NavLink
+              key={item.name}
+              to={item.link ? item.link : linkify(item.name)}
+              className="Link NavLink"
+            >
               {item.name}
             </NavLink>
           );
         })}
+        <hr />
       </div>
       <div className="d-flex justify-content-center">
         <div className="Content" id="Content">
